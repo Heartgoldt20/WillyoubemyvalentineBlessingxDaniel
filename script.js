fe0941a02c329348640b4786989ee3dd0c33736c
Daniel Ideho <59232190+Heartgoldt20@users.noreply.github.com>
2025-02-07 11:58:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const messages = [
     "Are you sure?",
     "Really sure??",
     "Are you positive?",
-    "Pookie please...",
+    "I'll start speaking finnish >:(",
     "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
+    "Ole hyvä.",
+    "Apparently it is friendsday in Finland, so Ole hyvä Ystävänpäivä",
+    "Please.",
     "Ok fine, I will stop asking...",
     "Just kidding, say yes please! ❤️"
 ];
